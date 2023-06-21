533f80fff75132e20546725c59c6c5cd44da2ba8
Ben Haig <bdhaig@gmail.com>
2023-06-21 16:16:16 -0400

    Lesson 6

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Anybody, Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
+const anybody = Anybody({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -15,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={anybody.className}>
         <h2>Layout</h2>
         {children}</body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,18 @@ import Counter from './Counter'
 
 export default function Home() {
   console.log('server component!')
+
+  const submit = async () => {
+    'use server'
+    console.log('server action!')
+  }
   return (
     <>
       <h1 className='text-xl'>Home /</h1>
       <Counter />
+      <form >
+        <input type='submit' className='--anybody' />
+      </form>
     </>
   )
 }
